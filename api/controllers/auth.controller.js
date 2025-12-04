e909ef14169f54738f7f0addf9a05e9bbff9f7a1
subhan513 <subhancs57@gmail.com>
2025-12-04 16:19:10 +0500

    Created Sign in Api Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js"
+import jwt from "jsonwebtoken"
 export const signup = async (req,res) => {
   const {username, email, password} = req.body;
   const user = User.findOne({email})
@@ -14,4 +15,24 @@ export const signup = async (req,res) => {
   } catch (error) {
     res.status(400).json({message  : "Failed to create the User", success : false})
   }
+}
+
+
+export const signin = async (req,res) => {
+   try {
+    const {email,password} = req.body;
+    const user = await User.findOne({email : email})
+    if(!User){
+        res.status(400).json({message : "User not Found" , success : false})
+    }
+    const VerifyPassword = bcrypt.compareSync(password,user.password)
+    if(!VerifyPassword){
+      res.status(500).json({message : "Failed to login the user", success : false})  
+    }
+    const token = jwt.sign({id :user._id},process.env.JWT_SECRET)
+     const {password:pass,...rest} = user._doc;
+    res.cookie("access_token",token,{httpOnly : true}).status(200).json(rest)
+   } catch (error) {
+    res.status(500).json({Message : "Failed to logged in user", success : false})
+   }
 }
