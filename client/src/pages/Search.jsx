604d6472be2f63c8d2bd489552ab6381c434312e
subhan513 <subhancs57@gmail.com>
2025-12-13 22:09:49 +0500

    completed  Listings Cards Functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import {useNavigate,useLocation} from "react-router-dom"
+import ListingItem from "../components/ListingItem";
 export default function Search() {
   const [sidebarData, setsidebarData] = useState({
     searchTerm : '',
@@ -201,6 +202,21 @@ const location = useLocation();
         <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>
           Listing results:
         </h1>
+         <div className="p-7 flex flex-wrap gap-4">
+           {!loading && listings.length === 0 && (
+            <p className='text-xl text-slate-700'>No listing found!</p>
+          )}
+            {loading && (
+            <p className='text-xl text-slate-700 text-center w-full'>
+              Loading...
+            </p>
+          )}
+           {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+        </div>
       </div>
     </div>
   );
