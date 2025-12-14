b2cf8b50d4940b0d07dd5c5449493c05291b878e
subhan513 <subhancs57@gmail.com>
2025-12-14 18:46:17 +0500

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  build: {
+    outDir: "dist",
+  },
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000', 
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
-        changeOrigin: true, 
+        changeOrigin: true,
       },
-    }
-  }
-})
+    },
+  },
+});
