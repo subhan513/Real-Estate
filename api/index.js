75a46968af68bbffc9470d2d684148decabd36b6
subhan513 <subhancs57@gmail.com>
2025-12-14 22:09:12 +0500

    done

--- api/index.js
+++ api/index.js
@@ -1,30 +1,27 @@
-// api/index.js
 import express from "express";
-import mongoose from "mongoose";
-import userRoutes from "../routes/user.route.js";
-import authRoutes from "../routes/auth.route.js";
-import ListingRouter from "../routes/listing.route.js";
+import mongoose  from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js"
+import ListingRouter from "./routes/listing.route.js"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO).then(()=>{
   console.log("Connected to the Database");
-}).catch((err) => {
-  console.log("Failed connect with Database", err);
-});
+  
+}).catch((err)=>{
+  console.log("Failed connnect with Database",err);
+})
+
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json({ limit: '5mb' }));
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/listing', ListingRouter);
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/listing',ListingRouter)
 
-app.get('/api/health', (req, res) => {
-  res.send({ success: "true" });
-});
-
-// Export the Express app
-export default app;
+app.listen(3000,()=>{
+  console.log("Server is listening on the port 3000");
+})
