971f515df60fa791a35117d3c8862de287cfa33e
subhan513 <subhancs57@gmail.com>
2025-12-14 19:05:34 +0500

    done

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    outDir: "dist",
-  },
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
