a0ab5801affc6e972a55e20378a41b425158c292
subhan513 <subhancs57@gmail.com>
2025-12-14 14:03:13 +0500

    add presinstalltion

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import ListingRouter from "./routes/listing.route.js"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO).then(()=>{
 }).catch((err)=>{
   console.log("Failed connnect with Database",err);
 })
+const _dirname = path.resolve();
+
 const app = express();
 app.use(cookieParser());
 app.use(express.json({ limit: '5mb' }));
@@ -20,6 +23,10 @@ app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 app.use('/api/listing',ListingRouter)
 
+app.use(express.static(path.join(_dirname,'client/dist')));
+app.get('*',(req,res)=>{
+   res.sendFile(path.join(_dirname,'client/dist','index.html'));
+});
 app.listen(3000,()=>{
   console.log("Server is listening on the port 3000");
 })
