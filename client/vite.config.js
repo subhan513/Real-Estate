b95f3f01709a56d98c6e3c025a5e33284f1fc75d
subhan513 <subhancs57@gmail.com>
2025-12-15 23:37:24 +0500

    updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://real-estate-tau-orpin.vercel.app/",
         secure: false,
         changeOrigin: true,
       },
